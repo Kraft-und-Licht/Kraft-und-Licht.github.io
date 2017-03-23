d5cdb15821f1a0fb26e4d6bea02af539d5840a7d
Panos Paganis <mallinanga@gmail.com>
2017-03-23 12:11:13 +0200

    WIP

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+(function () {
+    var header = $('.header')[0];
+
+    function changeHeader() {
+        var scrollTop;
+        if (header) {
+            scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            //header.classList.toggle('header-background', scrollTop >= 50 || document.body.classList.contains('nav-open'));
+            header.classList.toggle('header--scrolled', scrollTop >= ($('#content')[0].offsetTop - 50));
+        }
+    }
+
+    var didScroll = false;
+    $(window).scroll(function () {
+        didScroll = true;
+    });
+    setInterval(function () {
+        if (didScroll) {
+            didScroll = false;
+            changeHeader();
+        }
+    }, 100);
+    changeHeader();
+    $('a[href*=\\#]').on('click', function (event) {
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 500);
+        event.preventDefault();
+    });
+})();
+var loadScripts = function () {
+    var footer = $('footer');
+    $('body').css('opacity', '1');
+    //$('#content').css('margin-bottom', footer.outerHeight());
+    $('.menu__toggle').on('click', function () {
+        $('.menu').toggleClass('is-showing');
+    });
+};
+window.addEventListener('load', loadScripts);
